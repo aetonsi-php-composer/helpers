319b2892bf0c60aba40ce9c4d6685278f8e9afa5
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-09-22 15:31:44 +0200

    add and use plaintext helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,12 +1,18 @@
 <?php
 
+if (!function_exists('plaintext')) {
+    function plaintext()
+    {
+        \header('Content-Type: text/plain; charset=UTF-8');
+    }
+}
+
 if (!function_exists('vd')) {
     function vd()
     {
         $vars = \func_get_args();
-        if ($vars) {
-            foreach ($vars as $var) \var_dump($var);
-        }
+        if ($vars) foreach ($vars as $var) \var_dump($var);
+        plaintext();
         exit;
     }
 }
@@ -15,6 +21,7 @@ if (!function_exists('pr')) {
     function pr($arr)
     {
         \print_r($arr);
+        plaintext();
         exit;
     }
 }
