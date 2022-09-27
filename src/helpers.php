5c43204afaa6bc7a90239c4e83660038441bbf67
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-09-27 11:58:25 +0200

    add multiple parameters support for pr(), add comments, silence plaintext header warning

--- src/helpers.php
+++ src/helpers.php
@@ -1,30 +1,46 @@
 <?php
 
+
 if (!function_exists('plaintext')) {
+    /**
+     * Sets the content type to plain text.
+     */
     function plaintext()
     {
         \header('Content-Type: text/plain; charset=UTF-8');
     }
 }
 
+
 if (!function_exists('vd')) {
+    /**
+     * var_dumps all the given arguments, then terminates the program. Also prints the callee's file/function/line.
+     *
+     * @param ...mixed variables
+     */
     function vd()
     {
-        plaintext();
+        @plaintext(); // as this is just an helper function, silence possible "header already sent" warnings
         $dbbt = \debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0];
-        echo "{ $dbbt[file]:$dbbt[line] }\n";
+        echo "{ $dbbt[file] @ $dbbt[function]():$dbbt[line] }\n";
         foreach (\func_get_args() as $var) \var_dump($var);
         exit;
     }
 }
 
+
 if (!function_exists('pr')) {
-    function pr($arr)
+    /**
+     * print_rs all the given arguments, then terminates the program. Also prints the callee's file/function/line.
+     *
+     * @param ...mixed arrays
+     */
+    function pr()
     {
-        plaintext();
+        @plaintext(); // as this is just an helper function, silence possible "header already sent" warnings
         $dbbt = \debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0];
-        echo "{ $dbbt[file]:$dbbt[line] }\n";
-        \print_r($arr);
+        echo "{ $dbbt[file] @ $dbbt[function]():$dbbt[line] }\n";
+        foreach (\func_get_args() as $var) \print_r($var);
         exit;
     }
 }
